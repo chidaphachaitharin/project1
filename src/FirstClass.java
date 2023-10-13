bfa81680dbf22431b01b1a6e97ba4e86f537c0dc
chidaphachaitharin <jidapa17189@gmail.com>
2023-10-13 13:40:49 +0200

    my first commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,8 @@
 public class FirstClass {
     public static void main(String[] args) {
+
+        double weight = input.nextDouble();
+        double mars = (weight * .38);
         System.out.println("Hello Vistula");
 
 
