b9821c94e33a31fd83180d405420e985486a3d72
chidaphachaitharin <jidapa17189@gmail.com>
2023-10-13 13:42:30 +0200

    my first commit

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,9 +1,13 @@
 public class FirstClass {
     public static void main(String[] args) {
 
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("How many pounds (lbs) do you weigh? : ") ;
         double weight = input.nextDouble();
         double mars = (weight * .38);
-        System.out.println("Hello Vistula");
+
+        System.out.print("You weigh " + mars + " lbs on Mars \n");
 
 
     }
